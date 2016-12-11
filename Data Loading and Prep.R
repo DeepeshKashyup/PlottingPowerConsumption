d4c09438d9a793311e50c24c80ec31e4e6b89bba
DeepeshKashyup <deepesh.kashyup@gmail.com>
2016-12-11 11:58:09 -0500

    Adding file to download and clean data

---
+++ Data Loading and Prep.R
@@ -0,0 +1,11 @@
+if(!file.exists("exdata-Fdata-Fhousehold_power_consumption.zip")){
+  temp<-tempfile()
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+  f <- unzip(temp)
+  unlink(temp)
+}
+
+rawdf <-read.table(f,header=TRUE,sep=";",na.strings = "?")
+mydf <-rawdf[rawdf$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(mydf$Date, mydf$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+df<- cbind(SetTime,mydf)
