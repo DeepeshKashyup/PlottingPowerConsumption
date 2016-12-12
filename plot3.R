3fd709022707546b1d93716b18641bcc1d7572e8
DeepeshKashyup <deepesh.kashyup@gmail.com>
2016-12-11 19:07:56 -0500

    added code to download and transform data in each plotting file

--- plot3.R
+++ plot3.R
@@ -1,4 +1,17 @@
 plot3<- function(){
+#code to download and transform data 
+	if(!file.exists("exdata-Fdata-Fhousehold_power_consumption.zip")){
+	  temp<-tempfile()
+	  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+	  f <- unzip(temp)
+	  unlink(temp)
+	}
+
+	rawdf <-read.table(f,header=TRUE,sep=";",na.strings = "?")
+	mydf <-rawdf[rawdf$Date %in% c("1/2/2007","2/2/2007"),]
+	SetTime <-strptime(paste(mydf$Date, mydf$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+	df<- cbind(SetTime,mydf)
+
   plot(df$SetTime,df$Sub_metering_1,type="n",ylab="Energy Sub Metering",xlab= "")
   points(df$SetTime,df$Sub_metering_1,type="l")
   points(df$SetTime,df$Sub_metering_2,type="l",col="red")
